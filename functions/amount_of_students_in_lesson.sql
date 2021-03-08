fa3a0365fca963dfdb232add1f9abaadc300b5ce
oliverlowbob <oliverlowbob@live.dk>
2021-03-08 13:03:50 +0100

    fix amount of students in lesson

--- functions/amount_of_students_in_lesson.sql
+++ functions/amount_of_students_in_lesson.sql
@@ -10,18 +10,21 @@ CREATE DEFINER=`root`@`localhost` FUNCTION `amount_of_students_in_lesson`(
 ) RETURNS int
 BEGIN
 	DECLARE amount_students INT;
-    SET amount_students = 0;
+    	SET amount_students = 0;
     
 	SELECT count(l.user_iduser)
 	INTO amount_students
 	FROM lesson AS l, role as r, user_role as ur
 	WHERE l.class_idclass = class_id
-    AND ur.user_iduser = l.user_iduser
+    	AND ur.user_iduser = l.user_iduser
+    	AND ur.role_idrole = r.idrole
 	AND r.idrole = 2;
 	RETURN amount_students;
 END$$
 
 DELIMITER ;
+
+
 
 
 
--- populate_database.sql
+++ populate_database.sql
@@ -6,6 +6,7 @@ INSERT INTO `scool_protocol`.`user` (`iduser`, `school_idschool`, `first_name`, 
 INSERT INTO `scool_protocol`.`user` (`iduser`, `school_idschool`, `first_name`, `last_name`, `email`, `password`) VALUES ('2', '1', 'Jonas', 'Hansen', 'test2', 'test');
 INSERT INTO `scool_protocol`.`user` (`iduser`, `school_idschool`, `first_name`, `last_name`, `email`, `password`) VALUES ('3', '1', 'Kim', 'Madsen', 'test3', 'test');
 INSERT INTO `scool_protocol`.`user` (`iduser`, `school_idschool`, `first_name`, `last_name`, `email`, `password`) VALUES ('4', '1', 'Duktur', 'Mufti', 'test4', 'test');
+INSERT INTO `scool_protocol`.`user` (`iduser`, `school_idschool`, `first_name`, `last_name`, `email`, `password`) VALUES ('5', '1', 'Tomas', 'Kongen', 'test5', 'test');
 
 INSERT INTO `scool_protocol`.`role` (`idrole`, `role`) VALUES ('1', 'ADMIN');
 INSERT INTO `scool_protocol`.`role` (`idrole`, `role`) VALUES ('2', 'STUDENT');
@@ -15,6 +16,7 @@ INSERT INTO `scool_protocol`.`user_role` (`user_iduser`, `role_idrole`) VALUES (
 INSERT INTO `scool_protocol`.`user_role` (`user_iduser`, `role_idrole`) VALUES ('2', '3');
 INSERT INTO `scool_protocol`.`user_role` (`user_iduser`, `role_idrole`) VALUES ('3', '2');
 INSERT INTO `scool_protocol`.`user_role` (`user_iduser`, `role_idrole`) VALUES ('4', '3');
+INSERT INTO `scool_protocol`.`user_role` (`user_iduser`, `role_idrole`) VALUES ('5', '2');
 
 INSERT INTO `scool_protocol`.`course` (`idcourse`, `name`) VALUES ('1', 'Software Development');
 INSERT INTO `scool_protocol`.`course` (`idcourse`, `name`) VALUES ('2', 'Design');
@@ -24,5 +26,6 @@ INSERT INTO `scool_protocol`.`class` (`idclass`, `course_idcourse`, `start`, `en
 INSERT INTO `scool_protocol`.`lesson` (`user_iduser`, `class_idclass`) VALUES ('2', '1');
 INSERT INTO `scool_protocol`.`lesson` (`user_iduser`, `class_idclass`) VALUES ('3', '1');
 INSERT INTO `scool_protocol`.`lesson` (`user_iduser`, `class_idclass`) VALUES ('4', '1');
+INSERT INTO `scool_protocol`.`lesson` (`user_iduser`, `class_idclass`) VALUES ('5', '1');
 
 INSERT INTO `scool_protocol`.`attendance_key` (`idattendance_key`, `lesson_user_iduser`, `lesson_class_idclass`) VALUES ('"1"', '2', '1');
