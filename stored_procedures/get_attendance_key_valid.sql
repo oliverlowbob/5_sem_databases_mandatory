3a080ae45c4e33c64a6ec55bf35c42620e0a86b4
oliverlowbob <oliverlowbob@live.dk>
2021-05-01 19:00:41 +0200

    fix in out

--- stored_procedures/get_attendance_key_valid.sql
+++ stored_procedures/get_attendance_key_valid.sql
@@ -4,7 +4,7 @@ DROP procedure IF EXISTS `get_attendance_key_valid`;
 DELIMITER $$
 USE `scool_protocol`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_attendance_key_valid`(
-	key_id VARCHAR(255), out isValid INT(1)
+	in key_id VARCHAR(255), out isValid INT(1)
 )
 BEGIN
 	select check_attendance_key_valid(key_id) into isValid;
