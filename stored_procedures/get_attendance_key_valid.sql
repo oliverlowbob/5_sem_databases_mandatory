c21f7952d0385cf0c8694440d84f92487a8e319c
oliverlowbob <oliverlowbob@live.dk>
2021-05-01 18:58:22 +0200

    fix SP

--- stored_procedures/get_attendance_key_valid.sql
+++ stored_procedures/get_attendance_key_valid.sql
@@ -4,10 +4,10 @@ DROP procedure IF EXISTS `get_attendance_key_valid`;
 DELIMITER $$
 USE `scool_protocol`$$
 CREATE DEFINER=`root`@`localhost` PROCEDURE `get_attendance_key_valid`(
-	key_id VARCHAR(255)
+	key_id VARCHAR(255), out isValid INT(1)
 )
 BEGIN
-	select check_attendance_key_valid(key_id);
+	select check_attendance_key_valid(key_id) into isValid;
 END$$
 
 DELIMITER ;
